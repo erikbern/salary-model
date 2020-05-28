34d525e408d0886c8582737f25a0641e1fa59b19
Erik Bernhardsson <mail@erikbern.com>
2020-05-27 22:06:06 -0400

    lots of refactoring

--- plot.py
+++ plot.py
@@ -2,11 +2,21 @@ import contextlib, math, matplotlib, numpy, random
 from matplotlib import pyplot
 import scipy.optimize
 
-data = [(300, 250), (325, 293), (437, 325), (469, 330), (855, 431), (880, 463), (1032, 463), (1091, 520), (1130, 631), (1332, 868), (1622, 1071), (1650, 1177), (1676, 1285), (1701, 1388), (1783, 1401), (1824, 1501), (1874, 1564), (2100, 1637), (2310, 1675), (2446, 1781)]
+data = [(737, 600), (780, 555), (895, 585), (1032, 763), (1091, 740), (1230, 731), (1332, 968), (1422, 871), (1600, 877), (1676, 1085), (1751, 988), (1803, 1101), (1924, 985), (2000, 1064), (2200, 1137), (2310, 1375), (2446, 1321)]
 m = 2500
+
+# Fit curve through the points
+fn_0 = lambda x, alpha, beta: alpha*x**beta
+res = scipy.optimize.minimize(lambda args: sum((y - fn_0(x, *args))**2 for x, y in data), (0, 0))
+fn = lambda x: fn_0(x, *res.x)
+lo, hi = 0.9, 1.2
+fn_lo = lambda x: numpy.minimum(x, lo*fn(x))
+fn_hi = lambda x: numpy.minimum(x, hi*fn(x))
+xs = numpy.linspace(0, m, 1000)
 
 @contextlib.contextmanager
 def plot(fignr, title):
+    print('Plotting', fignr)
     pyplot.figure(figsize=(5, 4))
     # pyplot.grid(True, 'both', 'both')
     pyplot.gca().set_xticks([])
@@ -21,49 +31,74 @@ def plot(fignr, title):
     pyplot.annotate('salary', (0, 0), (0, m), arrowprops=arrowprops, ha='center', va='bottom')
     yield
     pyplot.gca().set_aspect('equal', adjustable='box')
-    pyplot.figtext(0.5, 0.02, 'Figure %d: %s' % (fignr, title), ha='center', style='italic', fontsize='small')
+    pyplot.figtext(0.5, 0.02, 'Figure %d: %s' % (fignr, title), ha='center', style='italic', fontsize='x-small')
     pyplot.tight_layout()
     pyplot.savefig('figure-%d.png' % fignr, dpi=600)
 
 # Base scatter
-with plot(1, 'Distribution of salaries at BananaLovers'):
+with plot(1, 'Distribution of salaries at CrazyBananaCo'):
     pyplot.scatter([x for x, y in data], [y for x, y in data], alpha=0.75, s=50, color='tab:blue')
+
+# The market
+with plot(2, 'Salaries aligned with the market rate and replacement cost at CrazyBananaCo'):
+    pyplot.scatter([x for x, y in data], [y for x, y in data], alpha=0.75, s=50, color='tab:blue')
+    pyplot.fill_between(xs, fn_lo(xs), fn_hi(xs), linewidth=0, alpha=0.3, color='tab:purple')
+    pyplot.annotate('market salary\nrange', (m, (fn_lo(m) + fn_hi(m))/2), ha='left', va='center', color='tab:purple')
+    pyplot.fill_between(xs, xs*0, fn_lo(xs), linewidth=0, alpha=0.3, color='tab:cyan')
+    pyplot.annotate('below\nmarket\nrate', (m, fn_lo(m)/2), ha='left', va='center', color='tab:cyan')
+    pyplot.fill_between(xs, fn_hi(xs), xs, linewidth=0, alpha=0.3, color='tab:red')
+    pyplot.annotate('above\nreplacement\ncost', (m, (m + fn_hi(m))/2), ha='left', va='center', color='tab:red')
+    pyplot.fill_between(xs, xs, [m for x in xs], linewidth=0, alpha=0.5, color='tab:red')
+    pyplot.annotate('negative value add', (m/2, m), ha='center', va='bottom', color='tab:red')
+
+xs_sorted = sorted(x for x, y in data)
+ys_sorted = sorted(y for x, y in data)
+cons_data = [(x, y) for x, y in zip(xs_sorted, ys_sorted)]
 
 # New hire
-with plot(2, 'New potential candidate at BananaLovers'):
-    pyplot.scatter([x for x, y in data], [y for x, y in data], alpha=0.75, s=50, color='tab:blue')
-    x, y = 750, 600
-    pyplot.scatter([x], [y], color='tab:green', alpha=0.75)
-    pyplot.annotate('candidate', (x, y), ha='right', va='top', color='tab:green')
-    pyplot.plot([x, x], [y, x], color='tab:olive')
-    pyplot.annotate('value surplus', (x, x), ha='left', va='top', color='tab:olive')
+with plot(3, 'Distribution of salaries at SuperFairCo'):
+    pyplot.scatter([x for x, y in cons_data], [y for x, y in cons_data], alpha=0.75, s=50, color='tab:blue')
+
+# New hire
+with plot(4, 'New potential candidate at SuperFairCo'):
+    pyplot.scatter([x for x, y in cons_data], [y for x, y in cons_data], alpha=0.75, s=50, color='tab:blue')
+    new_x, new_y = 1600, 1200
+    pyplot.scatter([new_x], [new_y], color='tab:green', alpha=0.75)
+    pyplot.annotate('candidate', (new_x, new_y), ha='right', va='top', color='tab:green')
+    pyplot.plot([new_x, new_x], [new_y, new_x], color='tab:olive')
+    pyplot.annotate('value surplus', (new_x, new_x), ha='left', va='top', color='tab:olive')
 
 # New hire inconsistencies
-with plot(3, 'Inconsistency cost of hiring the new candidate'):
-    pyplot.scatter([x for x, y in data], [y for x, y in data], alpha=0.75, s=50, color='tab:blue')
-    pyplot.scatter([x], [y], color='tab:green', alpha=0.75)
-    pyplot.plot([x, m], [y, y], color='black', linestyle=':')
-    xs, ys = [], []
-    for x2, y2 in data:
-        if x2 > x and y2 < y:
-            pyplot.annotate(None, (x2, y2), (x2, y), arrowprops=dict(arrowstyle='<|-', color='tab:red', shrinkA=0, shrinkB=0))
-            xs.append(x2)
-            ys.append(y2)
-    pyplot.annotate('inconsistencies', (numpy.median(xs), numpy.min(ys)), ha='center', va='bottom', color='tab:red')
+with plot(5, 'Inconsistency cost of hiring the new candidate at SuperFairCo'):
+    pyplot.scatter([x for x, y in cons_data], [y for x, y in cons_data], alpha=0.75, s=50, color='tab:blue')
+    pyplot.scatter([new_x], [new_y], color='tab:green', alpha=0.75)
+    pyplot.plot([new_x, m], [new_y, new_y], color='black', linestyle=':')
+    i_xs, i_ys = [], []
+    for x2, y2 in cons_data:
+        if x2 > new_x and y2 < new_y:
+            pyplot.annotate(None, (x2, y2), (x2, new_y), arrowprops=dict(arrowstyle='<|-', color='tab:red', shrinkA=0, shrinkB=0))
+            i_xs.append(x2)
+            i_ys.append(y2)
+    pyplot.scatter(i_xs, [new_y for y in i_ys], alpha=0.5, s=50, color='tab:cyan')
+    pyplot.annotate('inconsistencies', (numpy.median(i_xs), numpy.min(i_ys)), ha='center', va='bottom', color='tab:red')
 
 # New hire range
-with plot(4, 'Consistency-based salary range for the new hire'):
-    pyplot.scatter([x for x, y in data], [y for x, y in data], alpha=0.75, s=50, color='tab:blue')
-    min_y = max(y2 for x2, y2 in data if x2 < x)
-    max_y = min(y2 for x2, y2 in data if x2 > x)
-    pyplot.scatter([x, x], [min_y, max_y], color='tab:green', alpha=0.75)
-    pyplot.plot([x, x], [min_y, max_y], color='tab:green', alpha=0.75)
-    pyplot.annotate('salary range', (x, (min_y+max_y)/2), ha='left', va='center', color='tab:green')
+with plot(6, 'Consistency-based salary range for the new hire at SuperFairCo'):
+    pyplot.scatter([x for x, y in cons_data], [y for x, y in cons_data], alpha=0.75, s=50, color='tab:blue')
+    min_y = max(y2 for x2, y2 in cons_data if x2 < new_x)
+    max_y = min(y2 for x2, y2 in cons_data if x2 > new_x)
+    pyplot.scatter([new_x, new_x], [min_y, max_y], color='tab:green', alpha=0.75)
+    pyplot.plot([new_x, new_x], [min_y, max_y], color='tab:green', alpha=0.75)
+    pyplot.annotate('salary range', (new_x, (min_y+max_y)/2), ha='left', va='center', color='tab:green')
 
-new_data = [(737, 595), (780, 563), (855, 531), (1032, 763), (1091, 740), (1230, 731), (1332, 968), (1422, 871), (1650, 877), (1676, 1085), (1701, 988), (1803, 1101), (1924, 975), (2000, 1064), (2200, 1137), (2310, 1375), (2446, 1321)]
+new_data = [(x, min(y, y+3*(y-fn(x)))) for x, y in data]
 
-# Salary calibration
-with plot(5, 'Idealized salary calibration to bring the salaries to consistency'):
+# New hire
+with plot(7, 'Distribution of salaries at MegaHyperCo'):
+    pyplot.scatter([x for x, y in new_data], [y for x, y in new_data], alpha=0.75, s=50, color='tab:blue')
+
+# Salary calibration to consistency
+with plot(8, 'Idealized salary calibration to bring the salaries to consistency at MegaHyperCo'):
     pyplot.scatter([x for x, y in new_data], [y for x, y in new_data], alpha=0.75, s=50, color='tab:blue')
     adjusted_data = []
     for x, y in new_data:
@@ -73,25 +108,26 @@ with plot(5, 'Idealized salary calibration to bring the salaries to consistency'
             adjusted_data.append((x, min_y))
     pyplot.scatter([x for x, y in adjusted_data], [y for x, y in adjusted_data], alpha=0.5, s=50, color='tab:cyan')
 
-with plot(6, 'Idealized salary calibration to bring the salaries to the market rate'):
+# Salary calibration to market/replacement
+with plot(9, 'Calibrating salaries against market rate and replacement cost is at MegaHyperCo'):
     pyplot.scatter([x for x, y in new_data], [y for x, y in new_data], alpha=0.75, s=50, color='tab:blue')
-    fn = lambda args, x: args[0]*x**args[1]
-    args = scipy.optimize.minimize(lambda args: sum((y - fn(args, x))**2 for x, y in new_data), (0, 0)).x
-    f = 1.1
-    adjusted_data = [(x, f*fn(args, x)) for x, y in new_data]
-    for (x, y1), (_, y2) in zip(new_data, adjusted_data):
-        pyplot.annotate(None, (x, y1), (x, y2), arrowprops=dict(arrowstyle='<|-', color='tab:red', shrinkA=0, shrinkB=0))
-    xs = list(range(0, m))
-    pyplot.plot(xs, [f*fn(args, x) for x in xs], linestyle=':', color='tab:purple')
-    pyplot.annotate('market salary', (m, fn(args, m)), ha='left', va='center', color='tab:purple')
+    pyplot.fill_between(xs, fn_lo(xs), fn_hi(xs), linewidth=0, alpha=0.3, color='tab:purple')
+    pyplot.annotate('market salary\nrange', (m, (fn_lo(m) + fn_hi(m))/2), ha='left', va='center', color='tab:purple')
+    for x, y in new_data:
+        if y < fn_lo(x):
+            pyplot.annotate(None, (x, y), (x, fn_lo(x)), arrowprops=dict(arrowstyle='<|-', color='tab:red', shrinkA=0, shrinkB=0))
+            pyplot.scatter([x], [fn_lo(x)], alpha=0.5, s=50, color='tab:cyan')
 
-with plot(7, 'Calibrating salaries against our guess of where the market is'):
+# Salary calibration to consistency
+with plot(10, 'Idealized salary calibration to bring the salaries to consistency and market at MegaHyperCo'):
     pyplot.scatter([x for x, y in new_data], [y for x, y in new_data], alpha=0.75, s=50, color='tab:blue')
-    lo, hi = 1.0, 1.2
-    pyplot.fill_between(xs, [lo*fn(args, x) for x in xs], [hi*fn(args, x) for x in xs],
-                        linewidth=0, alpha=0.3, color='tab:purple')
-    pyplot.annotate('market salary\nrange', (m, (lo+hi)/2*fn(args, m)), ha='left', va='center', color='tab:purple')
-    adjusted_data = [(x, fn(args, x)) for x, y in new_data]
-    for (x, y1), (_, y2) in zip(new_data, adjusted_data):
-        if y1 < lo*y2:
-            pyplot.annotate(None, (x, y1), (x, lo*y2), arrowprops=dict(arrowstyle='<|-', color='tab:red', shrinkA=0, shrinkB=0))
+    pyplot.fill_between(xs, fn_lo(xs), fn_hi(xs), linewidth=0, alpha=0.3, color='tab:purple')
+    pyplot.annotate('market salary\nrange', (m, (fn_lo(m) + fn_hi(m))/2), ha='left', va='center', color='tab:purple')
+    adjusted_data = []
+    for x, y in new_data:
+        min_y = max([y2 for x2, y2 in new_data if x2 <= x])
+        new_y = max(min_y, fn_lo(x))
+        if new_y > y:
+            pyplot.annotate(None, (x, y), (x, new_y), arrowprops=dict(arrowstyle='<|-', color='tab:red', shrinkA=0, shrinkB=0))
+            adjusted_data.append((x, new_y))
+    pyplot.scatter([x for x, y in adjusted_data], [y for x, y in adjusted_data], alpha=0.5, s=50, color='tab:cyan')
